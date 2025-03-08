c58bcae9127524db8e4ade88404d74bbc698ddb9
Your Name <your.email@example.com>
2025-03-08 23:41:34 +0200

    adding collision with asteroids

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame 
+import time
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
 
 def main():
     
@@ -9,13 +13,16 @@ def main():
     pygame.display.set_caption("My Asteroids Game")
     clock = pygame.time.Clock()
    
-   
-    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) #define player to be in the middle of the screen
+
+    Asteroid.containers = (asteroids, drawable, updatable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     dt = 0 
     
@@ -24,12 +31,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        updatable.update(dt)    
+        updatable.update(dt)     
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+                
+
         screen.fill(BG_COLOR)
 
         for obj in drawable:
             obj.draw(screen)
-            
+
         pygame.display.flip()
 
         #limit the framerate to 60fps
