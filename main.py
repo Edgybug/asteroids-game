8bf0fa0c38ca6541412b7eed15f1a8eafb922cf2
Your Name <your.email@example.com>
2025-03-08 22:25:09 +0200

    adding static player in the middle of the screen

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import pygame 
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("My Asteroids Game")
-    
-    while True:
+    running = True
+
+    dt = 0 
+    clock = pygame.time.Clock()
+     
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
             
         screen.fill(BG_COLOR)
+        player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) #define player to be in the middle of the screen
+        player.draw(screen)
         pygame.display.flip()
+        clock.tick(60)
+        
+    dt = clock.tick(60) / 1000 
+    print(dt)
+
+    
 
 if __name__ == "__main__":
     main()
